083e936eae5c90876429f4bd4e4b95ebf85510f1
= <=>
2020-04-07 00:23:52 +0530

    Roles

--- src/main/java/dailyhunt/internship/endpoints/AuthEndpoints.java
+++ src/main/java/dailyhunt/internship/endpoints/AuthEndpoints.java
@@ -3,26 +3,28 @@ package dailyhunt.internship.endpoints;
 import dailyhunt.internship.clientmodels.request.LoginForm;
 import dailyhunt.internship.clientmodels.request.SignUpForm;
 import dailyhunt.internship.clientmodels.response.JwtResponse;
+import dailyhunt.internship.entities.Role;
 import dailyhunt.internship.exceptions.BadRequestException;
 import dailyhunt.internship.repositories.RoleRepository;
 import dailyhunt.internship.security.jwt.JwtProvider;
 import dailyhunt.internship.services.interfaces.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(AuthEndpoints.BASE_URL)
 public class AuthEndpoints {
     static final String BASE_URL = "/api/v1/auth";
+    private final RoleRepository roleRepository;
 
     private final AuthenticationManager authenticationManager;
 
@@ -30,7 +32,8 @@ public class AuthEndpoints {
 
     private final JwtProvider jwtProvider;
 
-    public AuthEndpoints(AuthenticationManager authenticationManager, UserService userService, JwtProvider jwtProvider) {
+    public AuthEndpoints(RoleRepository roleRepository, AuthenticationManager authenticationManager, UserService userService, JwtProvider jwtProvider) {
+        this.roleRepository = roleRepository;
         this.authenticationManager = authenticationManager;
         this.userService = userService;
         this.jwtProvider = jwtProvider;
@@ -56,5 +59,11 @@ public class AuthEndpoints {
     public ResponseEntity<String> registerUser(@Valid @RequestBody SignUpForm signUpRequest) throws BadRequestException {
         userService.saveUser(signUpRequest);
         return ResponseEntity.ok().body("User registered successfully!");
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<String> getRoles() throws BadRequestException {
+        List<Role> roles = roleRepository.findAll();
+        return ResponseEntity.ok().body(roles.toString());
     }
 }
