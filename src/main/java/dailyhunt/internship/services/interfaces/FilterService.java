d40a570bde7e0a75d5e7ebea0cc3a5030f83416a
singh3navdeep <navdeep3singh@yahoo.com>
2020-04-09 18:19:27 +0530

    filter functionality(single and group)

--- src/main/java/dailyhunt/internship/services/interfaces/FilterService.java
+++ src/main/java/dailyhunt/internship/services/interfaces/FilterService.java
@@ -1,5 +1,6 @@
 package dailyhunt.internship.services.interfaces;
 
+import dailyhunt.internship.clientmodels.request.DateFilter;
 import dailyhunt.internship.clientmodels.request.FilterForm;
 import dailyhunt.internship.entities.News;
 
@@ -8,4 +9,14 @@ import java.util.List;
 public interface FilterService {
 
      List<News> filter(FilterForm filterForm);
+
+     List<News> filterByKeyword(String keyword);
+
+     List<News> filterByDateRange(DateFilter dateFilter);
+
+     List<News> filterByTag(List<String> tags);
+
+     List<News> filterByGenre(List<Long> genreIds);
+
+     List<News> filterByLocality(List<Long> localityIds);
 }
