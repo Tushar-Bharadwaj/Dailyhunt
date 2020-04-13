5418e3982e5448e3962ddcfee66e3d5e7095d09e
= <=>
2020-04-13 13:20:22 +0530

    Data Loader

--- src/main/java/dailyhunt/internship/DataLoader.java
+++ src/main/java/dailyhunt/internship/DataLoader.java
@@ -1,39 +1,39 @@
-//package dailyhunt.internship;
-//
-//import dailyhunt.internship.entities.Role;
-//import dailyhunt.internship.enums.RoleName;
-//import dailyhunt.internship.repositories.RoleRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.ApplicationArguments;
-//import org.springframework.boot.ApplicationRunner;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Optional;
-//
-//@Component
-//public class DataLoader implements ApplicationRunner {
-//
-//    private RoleRepository roleRepository;
-//
-//    @Autowired
-//    public DataLoader(RoleRepository roleRepository) {
-//        this.roleRepository = roleRepository;
-//    }
-//
-//    public void run(ApplicationArguments args) {
-//        Optional<Role> roleUser = roleRepository.findByName(RoleName.ROLE_USER);
-//        if(!roleUser.isPresent())
-//            roleRepository.save(Role.builder()
-//                .name(RoleName.ROLE_USER).build());
-//
-//        Optional<Role> roleModerator = roleRepository.findByName(RoleName.ROLE_MODERATOR);
-//        if(!roleModerator.isPresent())
-//        roleRepository.save(Role.builder()
-//                .name(RoleName.ROLE_MODERATOR).build());
-//
-//        Optional<Role> roleAdmin = roleRepository.findByName(RoleName.ROLE_ADMIN);
-//        if(!roleAdmin.isPresent())
-//        roleRepository.save(Role.builder()
-//                .name(RoleName.ROLE_ADMIN).build());
-//    }
-//}
+package dailyhunt.internship;
+
+import dailyhunt.internship.entities.Role;
+import dailyhunt.internship.enums.RoleName;
+import dailyhunt.internship.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public DataLoader(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public void run(ApplicationArguments args) {
+        Optional<Role> roleUser = roleRepository.findByName(RoleName.ROLE_USER);
+        if(!roleUser.isPresent())
+            roleRepository.save(Role.builder()
+                .name(RoleName.ROLE_USER).build());
+
+        Optional<Role> roleModerator = roleRepository.findByName(RoleName.ROLE_MODERATOR);
+        if(!roleModerator.isPresent())
+        roleRepository.save(Role.builder()
+                .name(RoleName.ROLE_MODERATOR).build());
+
+        Optional<Role> roleAdmin = roleRepository.findByName(RoleName.ROLE_ADMIN);
+        if(!roleAdmin.isPresent())
+        roleRepository.save(Role.builder()
+                .name(RoleName.ROLE_ADMIN).build());
+    }
+}
